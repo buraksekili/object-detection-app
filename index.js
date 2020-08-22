134f8ba0b8ee0a8a49cfe2006af39351644e090d
buraksekili <buraksekili@gmail.com>
2020-08-23 01:00:19 +0300

    curl is replaced with axios

--- index.js
+++ index.js
@@ -3,19 +3,15 @@ const multer = require("multer");
 const FormData = require("form-data");
 const cors = require("cors");
 const fs = require("fs");
-const { Curl } = require("node-libcurl");
 const path = require("path");
-const curl = new Curl();
 const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-require("dotenv").config();
 
 app.use(cors());
 app.use(express.static("uploads"));
-
-var storedFilename = "";
 
 function serverLog(callerFuncName, message) {
     console.log(`${callerFuncName} => ${message}`);
@@ -26,9 +22,7 @@ const storage = multer.diskStorage({
         cb(null, "./uploads/");
     },
     filename: function (req, file, cb) {
-        storedFilename = file.originalname;
-
-        cb(null, storedFilename);
+        cb(null, file.originalname);
     },
 });
 
@@ -78,43 +72,18 @@ app.post("/send", upload.array("images"), (req, resp) => {
 
     axios(config)
         .then(function (response) {
-            fs.unlink("./uploads/" + storedFilename, (err) => {
+            fs.unlink("./uploads/" + file.originalname, (err) => {
                 if (err) {
                     serverLog("fs.unlink", `fs.unlink error: ${err}`);
                     return console.error("err");
                 }
             });
+            serverLog("/send", "response send successfully");
             resp.send(response.data.response[0].detections);
         })
         .catch(function (error) {
             console.log(error);
         });
-
-    // curl.setOpt(Curl.option.URL, process.env.ApiUrl);
-    // curl.setOpt(Curl.option.HTTPPOST, [
-    //     {
-    //         name: "images",
-    //         file: req.files[0].path,
-    //         type: req.files[0].mimetype,
-    //     },
-    // ]);
-
-    // curl.on("end", function (statusCode, body, headers) {
-    //     const newBody = JSON.parse(body);
-    //     fs.unlink("./uploads/" + storedFilename, (err) => {
-    //         if (err) {
-    //             serverLog("fs.unlink", `fs.unlink error: ${err}`);
-    //             return console.error("err");
-    //         }
-    //     });
-    //     resp.status(statusCode).send(newBody.response[0].detections);
-    // });
-    // curl.on("error", function (error) {
-    //     this.close();
-    //     serverLog("curl.on", `curl.on error: ${error}`);
-    //     resp.send(error);
-    // });
-    // curl.perform();
 });
 
 app.listen(port, () => {
